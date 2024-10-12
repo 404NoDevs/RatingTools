a3c5f03738c50cda4638cb46bd28d598a9dd4b2a
Overflow <chang_17600907339@163.com>
2024-10-13 01:22:05 +0800

    1

--- modules/base/base_suit_result.py
+++ modules/base/base_suit_result.py
@@ -36,10 +36,15 @@ class BaseSuitResultWindow(QWidget):
         self.setFocusPolicy(Qt.StrongFocus)
         self.move(*self.position)
 
-        # 创建界面UI
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        layout.addWidget(QLabel("当前方案："), 0, 0, 1, 1)
         self.programmeCombobox = QComboBox()
+        layout.addWidget(self.programmeCombobox, 0, 1, 1, 4)
         self.copyButton = QPushButton('复制得分')
-        self.posNameLabel = {}
+        layout.addWidget(self.copyButton, 0, 5, 1, 1)
+
         self.artifactNameLabel1 = {}
         self.artifactScoreLabel1 = {}
         self.artifactOwnerLabel1 = {}
@@ -47,41 +52,31 @@ class BaseSuitResultWindow(QWidget):
         self.artifactScoreLabel2 = {}
         self.artifactOwnerLabel2 = {}
         self.artifactScoreSubLabel = {}
-        for posItem in self.data.getPosName():
-            self.posNameLabel[posItem] = QLabel(posItem)
+        for index, posItem in enumerate(self.data.getPosName()):
+            layout.addWidget(QLabel(posItem), 1 + 3 * index + 1, 0, 2, 1)
+            layout.addWidget(QLabel("当前："), 1 + 3 * index + 1, 1, 1, 1)
             self.artifactNameLabel1[posItem] = QLabel("无装备")
-            self.artifactScoreLabel1[posItem] = QLabel("0")
+            layout.addWidget(self.artifactNameLabel1[posItem], 1 + 3 * index + 1, 2, 1, 1)
             self.artifactOwnerLabel1[posItem] = QLabel("0")
+            layout.addWidget(self.artifactOwnerLabel1[posItem], 1 + 3 * index + 1, 3, 1, 1)
+            self.artifactScoreLabel1[posItem] = QLabel("0")
+            layout.addWidget(self.artifactScoreLabel1[posItem], 1 + 3 * index + 1, 4, 1, 1)
+            layout.addWidget(QLabel("推荐："), 2 + 3 * index + 1, 1, 1, 1)
             self.artifactNameLabel2[posItem] = QLabel("无装备")
-            self.artifactScoreLabel2[posItem] = QLabel("0")
+            layout.addWidget(self.artifactNameLabel2[posItem], 2 + 3 * index + 1, 2, 1, 1)
             self.artifactOwnerLabel2[posItem] = QLabel("无人装备")
+            layout.addWidget(self.artifactOwnerLabel2[posItem], 2 + 3 * index + 1, 3, 1, 1)
+            self.artifactScoreLabel2[posItem] = QLabel("0")
+            layout.addWidget(self.artifactScoreLabel2[posItem], 2 + 3 * index + 1, 4, 1, 1)
             self.artifactScoreSubLabel[posItem] = QLabel("0")
-        self.equipTips = QLabel("将推荐装备全部标记")
-        self.equipTips.setStyleSheet("color:red;qproperty-alignment: 'AlignCenter';")
-        self.equipButton = QPushButton('全部装备')
+            layout.addWidget(self.artifactScoreSubLabel[posItem], 1 + 3 * index + 1, 5, 2, 1)
+            layout.addWidget(QLabel(" "), 4 + 3 * index, 0, 1, 4)  # 占位符
 
-        # 弹窗内容
-        layout = QGridLayout()
-        layout.addWidget(QLabel("当前方案："), 0, 0, 1, 1)
-        layout.addWidget(self.programmeCombobox, 0, 1, 1, 4)
-        layout.addWidget(self.copyButton, 0, 5, 1, 1)
-        counter = 0
-        for posItem in self.data.getPosName():
-            layout.addWidget(self.posNameLabel[posItem], 1 + 3 * counter + 1, 0, 2, 1)
-            layout.addWidget(QLabel("当前："), 1 + 3 * counter + 1, 1, 1, 1)
-            layout.addWidget(self.artifactNameLabel1[posItem], 1 + 3 * counter + 1, 2, 1, 1)
-            layout.addWidget(self.artifactOwnerLabel1[posItem], 1 + 3 * counter + 1, 3, 1, 1)
-            layout.addWidget(self.artifactScoreLabel1[posItem], 1 + 3 * counter + 1, 4, 1, 1)
-            layout.addWidget(QLabel("推荐："), 2 + 3 * counter + 1, 1, 1, 1)
-            layout.addWidget(self.artifactNameLabel2[posItem], 2 + 3 * counter + 1, 2, 1, 1)
-            layout.addWidget(self.artifactOwnerLabel2[posItem], 2 + 3 * counter + 1, 3, 1, 1)
-            layout.addWidget(self.artifactScoreLabel2[posItem], 2 + 3 * counter + 1, 4, 1, 1)
-            layout.addWidget(self.artifactScoreSubLabel[posItem], 1 + 3 * counter + 1, 5, 2, 1)
-            layout.addWidget(QLabel(" "), 4 + 3 * counter, 0, 1, 4)
-            counter += 1
-        layout.addWidget(self.equipTips, 100, 0, 1, 10)
-        layout.addWidget(self.equipButton, 101, 0, 1, 10)
-        self.setLayout(layout)
+        self.equipTipsLabel = QLabel("将推荐装备全部标记")
+        self.equipTipsLabel.setStyleSheet("color:red;qproperty-alignment: 'AlignCenter';")
+        layout.addWidget(self.equipTipsLabel, 100, 0, 1, 5)
+        self.equipButton = QPushButton('全部装备')
+        layout.addWidget(self.equipButton, 101, 0, 1, 5)
 
         # 注册事件
         self.programmeCombobox.currentIndexChanged.connect(self.programmeCurrentIndexChanged)
@@ -148,7 +143,7 @@ class BaseSuitResultWindow(QWidget):
     # 全部装备
     def allEquip(self):
         # print("全部装备")
-        self.equipTips.setText(self.character + "已全部装备")
+        self.equipTipsLabel.setText(self.character + "已全部装备")
         newArtifactsData = self.resultArray[self.programmeCombobox.currentIndex()]["combinationName"]
         self.data.setArtifactOwner(self.character, newArtifactsData)
         self.updateUI()
@@ -158,4 +153,4 @@ class BaseSuitResultWindow(QWidget):
         # 复制文本到剪贴板
         text_to_copy = "	".join(str(score) for score in self.score_array)
         pyperclip.copy(text_to_copy)
-        self.equipTips.setText("得分已复制到剪切板")
+        self.equipTipsLabel.setText("得分已复制到剪切板")
